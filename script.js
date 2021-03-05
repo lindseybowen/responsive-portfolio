706676ca5006b743cfeb38e60f04b9ce03db6acc
lindsey Bowen <lindseyjbowen@gmail.com>
2021-03-04 21:41:11 -0500

    started to add skills. Need photos and to add a few more skills

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
   
